47077dfbe1840aa73fbe17004f1ebcc3c8768fe6
bnjax3 <153530323+bnjax3@users.noreply.github.com>
2024-11-07 08:21:36 -0300

    esa
    aesd

--- palabrino/main.py
+++ palabrino/main.py
@@ -1,47 +1,25 @@
-import time
-import pyfirmata
 import interfaz
+import arduino
+import time
+import json
 
-board = pyfirmata.ArduinoMega('COM7')
-it = pyfirmata.util.Iterator(board)
-it.start()
 
-class led:
-    def __init__(self, pin):
-        self.pin = pin
-        self.port = board.digital[pin]
-        self.state = 2
-    
-                
-        
-class display:
-    def __init__(self, ledAmount):
-        self.leds = []
-        self.ledAmount = ledAmount
-        for i in range(2, ledAmount + 2):
-            self.leds.append(led(i))  #led.pin
-            
-    def refresh(self):
-        for led in self.leds:
-            if led.state == 0:
-                led.port.write(0)
-            if led.state == 1:
-                led.port.write(variablerandomquenecesitoparaquetitile)               
-            if led.state == 2:
-                led.port.write(1)
-            
-            
-screen = display(25)
+
+
+
+screen = arduino.display(25)
 running = True
-variablerandomquenecesitoparaquetitile = 0
-
 screen.leds[2].state = 0
 
+
+
+
+#  Main Loop
 while running:
     screen.refresh()
-    variablerandomquenecesitoparaquetitile+=1
-    if variablerandomquenecesitoparaquetitile > 1:
-        variablerandomquenecesitoparaquetitile = 0
+    blinkCounter+=1
+    if blinkCounter > 1:
+        blinkCounter = 0
     time.sleep(0.5)
 
 
