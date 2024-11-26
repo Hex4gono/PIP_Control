8903714153ce7e8eb99229cbb203a3b6ccd91580
Hex4gono <andradagezequiel@gmail.com>
2024-11-26 14:09:35 -0300

    abc

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from GUI.menu import Ui_MainWindow
 from GUI.nameprompt import Ui_Dialog
 from GUI.promptconfirmar import Ui_ConfirmarDialog
 from arduino.comunicador import arduino
+import btnsfunc
 
 with open("data/controllers.json", "r") as file:
     controles = json.load(file)
+    controles = dict(sorted(controles.items()))
 caracteres = ['mouseUp','mouseDown','mouseRight','mouseLeft','mouseClickLeft','mouseClickRight','up', 'down', 'right', 'left', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.', '-', '_', '!', '?', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '<', '>', '/', '\\', '|', '+', '=', ':', ';', '"', '\'']
 class ConfirmarDialog(QDialog):
     def __init__(self):
@@ -52,11 +54,6 @@ class MainWindow(QMainWindow):  #Clase MainWindow heredada de QMainWindow, que e
         self.ui = Ui_MainWindow() #crea una instancia de Ui_MainWindow class, la cual es la definición de la interfaz del usuario para la ventana principal.
         self.ui.setupUi(self) #llama al método setupUi() de la instancia Ui_MainWindow, para setear los componenetes de la interfaz del usuario dentro de main window.
         self.ui.CerrarButton.setGeometry(271,1,161,23)
-        """     
-        self.ui.CerrarButton.clicked.connect(self.close)#Boton para cerrar
-        self.ui.cargarButton
-        self.ui.guardarButton
-        """
         # Haces un array con todos los caracteres seleccionables y despues la pones de accion en todos los combo box
         global widgets 
         widgets = [self.ui.XNegCar,self.ui.XPosCar,self.ui.YNegCar,self.ui.YPosCar,self.ui.XNegCar_2,self.ui.XPosCar_2,self.ui.YNegCar_2,self.ui.YPosCar_2,self.ui.Boton1Sel,self.ui.Boton2Sel,self.ui.Boton3Sel,self.ui.Boton4Sel,self.ui.Boton5Sel,self.ui.Boton6Sel,self.ui.Boton7Sel,self.ui.Boton8Sel,self.ui.L3Sel,self.ui.R3Sel]
@@ -65,6 +62,17 @@ class MainWindow(QMainWindow):  #Clase MainWindow heredada de QMainWindow, que e
         global comboBox
         comboBox = self.ui.controlesComboBox
         comboBox.addItems(controles.keys())
+    def sortearControl(self):
+        if self.ui.SortButton.text == "A-Z":
+            self.ui.SortButton.setText("Z-A")
+            comboBox.clear()
+            controles = dict(sorted(controles.items(),reverse=True))
+            comboBox.addItems(controles.keys())
+        else:
+            self.ui.SortButton.setText("A-Z")
+            comboBox.clear()
+            controles = dict(sorted(controles.items()))
+            comboBox.addItems(controles.keys())
         
     def cargarControl(self):
         controlActual = controles[self.ui.controlesComboBox.currentText()]
@@ -75,14 +83,13 @@ class MainWindow(QMainWindow):  #Clase MainWindow heredada de QMainWindow, que e
         claves = ["XNegCar", "XPosCar", "YNegCar", "YPosCar","XNegCar_2", "XPosCar_2", "YNegCar_2", "YPosCar_2","Boton1Sel", "Boton2Sel", "Boton3Sel", "Boton4Sel","Boton5Sel", "Boton6Sel", "Boton7Sel", "Boton8Sel","L3Sel","R3Sel"]
         for i in range(len(widgets)):
             widgets[i].setCurrentText(controlActual.get(claves[i]))
-            
+  
+
     def guardarControl(self):
-        # Crear instancia del diálogo y abrirlo
-        dialogo = Dialog()  # Crear una instancia de Dialog
-        dialogo.exec_()  # Mostrar el diálogo de manera modal
+        dialogo = Dialog()
+        dialogo.exec_()  
         
-    def eliminarControl(self):   
-        # sin esto no funciona y no se porque
+    def eliminarControl(self):
         a = ConfirmarDialog() 
         a.exec_()  
     
