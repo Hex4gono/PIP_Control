581adf858d4c2f99f6124772bd4f415defd58410
bnjax3 <153530323+bnjax3@users.noreply.github.com>
2024-11-27 03:26:05 -0300

    ya casi

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,38 +1,66 @@
 
-int digitalPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-int analogPins[] = {A0, A1, A2, A3};
+int digitalPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+int analogPins[] = {A0, A1, A2, A4};
 
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   
-  for (int i = 0; i < sizeof(digitalPins); i++) {
-    pinMode(i, INPUT);
+  for (int i = 2; i < sizeof(digitalPins); i++) 
+  {
+    pinMode(i, INPUT_PULLUP);
   }
 }
 
-void loop() {
+void loop() 
+{
   String data = "[";
 
 
 
-  // Leer pines analógicos
-  for (int i = 0; i < sizeof(analogPins); i++) {
+  // Leer pines analógicos  (el sizeof / size of es porque aparentemente c++ lee el tamano en bytes y no cant de elementos)
+  for (int i = 0; i < sizeof(analogPins)  / sizeof(analogPins[0]) ; i++) 
+  {
     data += analogRead(analogPins[i]);
-    if (i < sizeof(analogPins) - 1) {
+    if (i < sizeof(analogPins) / sizeof(analogPins[0]) - 1) 
+    {
       data += ", "; 
     }
   }
 
-  // Leer pines digitales
-  for (int i = 0; i < sizeof(digitalPins); i++) {
-    data += digitalRead(digitalPins[i]);
-    if (i < sizeof(digitalPins) - 1 || sizeof(analogPins) > 0) {
-      data += ", "; 
+  // Leer pines digitales (deben ser revertidos porque uso la resistencia interna pullup)
+  for (int i = 2; i < sizeof(digitalPins)  / sizeof(digitalPins[0]); i++) 
+  {
+
+    data += ", ";
+    // invierte los valores por el pullup
+    if (digitalRead(i) == HIGH) 
+    {
+      data += "0";
+    } else {
+      data += "1";  
     }
+
   }
 
   data += "]";
   Serial.println(data);
   delay(100);
+  int valueA0 = analogRead(A0); // Leer A0
+  int valueA1 = analogRead(A1); // Leer A1
+/*
+// debug
+  Serial.print("A0: ");
+  Serial.print(1023 - valueA0);
+  Serial.print(" | A1: ");
+  Serial.println(1023 - valueA1);
+
+ // A3 dejo de funcionar de la maldita nada asi que tuve que remplazarlo
+  Serial.print("A2: ");
+  Serial.print(analogRead(A2));
+  Serial.print(" | A4: ");
+  Serial.println(analogRead(A4));
+*/
+  delay(30); 
 }
