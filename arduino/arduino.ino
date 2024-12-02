21ef3b4801b3a89dfaef4eb7d8e4209a9f21f9b6
bnjax3 <153530323+bnjax3@users.noreply.github.com>
2024-12-02 08:29:56 -0300

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,15 +1,15 @@
 
-int digitalPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-int analogPins[] = {A0, A1, A2, A4};
+int digitalPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+int analogPins[] = {A0, A1, A2, A4}; //a3 no anda
 
 
 void setup() 
 {
   Serial.begin(9600);
   
-  for (int i = 2; i < sizeof(digitalPins); i++) 
+  for (int i = 0; i < sizeof(digitalPins); i++) 
   {
-    pinMode(i, INPUT_PULLUP);
+    pinMode(digitalPins[i], INPUT_PULLUP);
   }
 }
 
@@ -19,10 +19,13 @@ void loop()
 
 
 
-  // Leer pines analógicos  (el sizeof / size of es porque aparentemente c++ lee el tamano en bytes y no cant de elementos)
+  // Leer pines analógicos  (el sizeof / size of es porque c++ lee el tamano en bytes y no cant de elementos)
   for (int i = 0; i < sizeof(analogPins)  / sizeof(analogPins[0]) ; i++) 
   {
-    //hace falta revertir las coordenadas de uno de los joysticks por la forma del circuito
+    // hace falta revertir las coordenadas de uno de los joysticks por la forma del circuito
+    // ahora tambien las coordenadas del otro
+
+    /*
     if (i == 0 || i == 1) // si estamos en la primera o segunda iteracion (js 1)
     {
       data += (1023 - analogRead(analogPins[i])) - 515;
@@ -30,30 +33,36 @@ void loop()
       data += analogRead(analogPins[i]) - 515;
     }
 
-    if (i < sizeof(analogPins) / sizeof(analogPins[0]) - 1);
+    */
+
+    data += (1023 - analogRead(analogPins[i])) - 515;
+
+    if (i < sizeof(analogPins) / sizeof(analogPins[0]) -1);
     {
       data += ", "; 
     }
   }
 
   // Leer pines digitales (deben ser revertidos porque uso la resistencia interna pullup)
-  for (int i = 2; i < sizeof(digitalPins)  / sizeof(digitalPins[0]); i++) 
+  for (int i = 0; i < sizeof(digitalPins)  / sizeof(digitalPins[0]); i++) 
   {
-
-    data += ", ";
     // invierte los valores por el pullup
-    if (digitalRead(i) == HIGH) 
+    if ((digitalRead(digitalPins[i]) == HIGH)) 
     {
       data += "0";
     } else {
       data += "1";  
     }
 
+    if (i < sizeof(digitalPins) / sizeof(digitalPins[0]) -1)
+    {
+      data += ", "; 
+    }
   }
 
   data += "]";
   Serial.println(data);
-  delay(100);
+  delay(20);
   int valueA0 = analogRead(A0); // Leer A0
   int valueA1 = analogRead(A1); // Leer A1
 /*
